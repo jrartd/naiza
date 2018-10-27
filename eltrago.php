3b671b9dd8a750c1fc3b408cf3d7fe761088106e
Your Name <you@example.com>
2018-10-26 19:18:09 -0500

    creacion de eltrago

---
+++ eltrago.php
@@ -0,0 +1,17 @@
+<?php 
+/*
+Template name: principal
+*/
+?>
+
+<?php 
+include "header.php";
+include "social.php";
+include "menu.php"; 
+include "container1.php";
+include "bio.php";
+include "video.php"; 
+# include "eventos.php";
+include "galeria.php"; 
+include "footer.php";
+?>
--- index.php
+++ index.php
@@ -10,7 +10,22 @@ include "social.php";
 include "menu.php"; 
 include "container1.php";
 include "bio.php";
-include "video.php"; 
+?>
+
+<!-- ready-->
+
+<div class="container-fluid" id="video" style="padding: 0px; margin-top:0px; margin-bottom: 0px; " id="lastvideo">
+  <div class="row" style="padding: 0px;">
+    <div class="col-sm-12" style="padding: 0px;">
+        <div class="embed-responsive embed-responsive-16by9">
+            <iframe style="padding-top: 0px;padding-bottom: 0px;" class="embed-responsive-item" src="https://www.youtube.com/embed/qJc7BMxgho0"></iframe>
+        </div>
+    </div>
+  </div>
+</div>
+
+
+<?php 
 # include "eventos.php";
 include "galeria.php"; 
 include "footer.php";
