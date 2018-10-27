8c48fb995cec2e0dd13ed361e682c9f643946d29
Your Name <you@example.com>
2018-10-26 20:31:48 -0500

    instagram

--- menu.php
+++ menu.php
@@ -15,22 +15,9 @@
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
         <li class="active"><a href="#bio">Biografía</a></li>
-        <li><a href="#video">Last video</a></li>
+        <li><a href="eltrago.php">El trago</a></li>
         <li><a href="#galeria">Galería</a></li>
       </a>
-        <!--
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">One more separated link</a></li>
-          </ul>
-        </li>-->
       </ul>
       <ul class="nav navbar-nav navbar-right">
         <li>
